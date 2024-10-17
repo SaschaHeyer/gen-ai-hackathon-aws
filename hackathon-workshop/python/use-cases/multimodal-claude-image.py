f698af34247266f9fc98debd8c7ae7df5d4388e4
Sascha Heyer <heyersaschastud@googlemail.com>
2024-10-17 15:18:53 +0200

    added additional use cases

--- hackathon-workshop/python/use-cases/multimodal-claude-image.py
+++ hackathon-workshop/python/use-cases/multimodal-claude-image.py
@@ -6,7 +6,7 @@ import base64
 bedrock_runtime = boto3.client('bedrock-runtime')
 
 # image size max 5MB
-with open("london.jpg", "rb") as image_file:
+with open("kindle.jpg", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
     base64_string = encoded_string.decode('utf-8')
     
@@ -31,7 +31,21 @@ payload = {
                     },
                     {
                         "type": "text",
-                        "text": "Write me a detailed description of this photo"
+                        "text": """
+                        Based on the image and the following product specification write a engaging product description in Dutch language.
+                        
+                        Display: 6.8-inch glare-free display, 300 ppi resolution
+                        Storage: 8 GB or 32 GB options
+                        Battery Life: Up to 10 weeks on a single charge
+                        Lighting: Adjustable warm light and auto-adjusting front light
+                        Waterproof: IPX8 rating, can withstand immersion in water
+                        Connectivity: Wi-Fi or Wi-Fi + Free Cellular Connectivity options
+                        Charging: USB-C for faster charging
+                        Weight: 205 grams
+                        Dimensions: 174 x 125 x 8.1 mm
+                        Materials: Made with 60% post-consumer recycled plastics
+                        Additional Features: Dark Mode, adjustable text size, and support for audiobooks via Bluetooth.
+                        """
                     }
                 ]
             }
